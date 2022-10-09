ae91475f59fc3d14b58cb1720ee5fd8b0fec3c8f
Semenov Sergei <dp120291ssv@gmail.com>
2022-10-09 20:52:01 +0300

    final

--- js/FormValidator.js
+++ js/FormValidator.js
@@ -1,5 +1,40 @@
 export default class FormValidator {
     constructor(selector) {
         this.form = document.querySelector(selector);
+        this.inputsWithErrors = new Set();
+
+        this.form.addEventListener('submit', e => {
+            e.preventDefault();
+
+            if(!this.hasError){
+                this.form.submit();
+            }
+        })
+    }
+
+    get hasError(){
+        return true;
+    }
+
+    register(selector, check){
+        const inputField = this.form.querySelector(selector);
+        const errorElement = inputField.closest('.input').querySelector('.input__error');
+
+        const execute = (hideErrors) => {
+            const {pass, error} = check(inputField.value, inputField);
+
+            if(!hideErrors){
+                errorElement.textContent = error || '';
+            }
+
+            if(!pass){
+                this.inputsWithErrors.add(inputField);
+            } else {
+                this.inputsWithErrors.remove(inputField);
+            }
+        }
+
+        inputField.addEventListener('change', () => execute());
+        execute(true);
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
-import FormValidator from "./FormValidator";
+import FormValidator from "./FormValidator.js";
 
 const fv = new FormValidator('#signup');
+
+function validateLength(value, inputField){
+    if(value.length === 0 || value.length > 5){
+        return {
+            pass: false,
+            error: 'Username must be between 1 - 5 characters.'
+        }
+    }
+
+    return {
+        pass: true
+    }
+}
+
+fv.register('#username', validateLength);
 
 window.fv = fv;
